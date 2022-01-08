90102ec88051cd9aad1d0d8be087b241ec448091
Daniel Agüero <daniel.esteban.aguero@gmail.com>
2022-01-08 17:02:24 -0300

    libros y autores clases prueba

---
+++ codigo.js
@@ -0,0 +1,25 @@
+/* function factorial(n) {
+    if (n % 1 == 0) {
+    if (n>0) {
+    return n*factorial(n-1);
+    } else {
+    return 1;
+    }
+    } else {
+    return -1;
+    }
+    }
+    QUnit.test( "Prueba función factorial", function( assert ) {
+        assert.equal(factorial(5),120, "Correcto el factorial de 5.");
+        assert.equal(factorial(6),720, "Correcto el factorial de 6.");
+        assert.equal(factorial(7),5040, "Correcto el factorial de 7.");
+        assert.equal(factorial(8),40321, "Correcto el factorial de 8.");
+    }); */
+/*     var miarray = ["hola" , "estamos", "haciendo", "una", "prueba"];
+    QUnit.test( "Prueba Array", function( assert ) {
+    assert.equal(miarray[0],"hola", "Correcto" );
+    assert.equal(miarray[1],"estamos", "Correcto" );
+    assert.equal(miarray[2],"haciendo", "Correcto" );
+    assert.equal(miarray[3],"una", "Correcto" );
+    assert.equal(miarray[4],"prueba", "Correcto" );
+    }); */
